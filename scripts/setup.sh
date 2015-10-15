c11e611d3b80451bddf6111ccafacaf06c91bb3f
unknown <ag.nanninga@gmail.com>
2015-10-15 11:11:43 +0200

    fixed sql import in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ echo "Creating voerr database"
 mysql -uroot -e "create database voerr;"
 
 echo "Creating voerr tables"
-mysql -uroot voerr < /flask/sql/voerr.sql
+mysql -uroot voerr < /voerr/sql/voerr.sql
 
 echo "Setup 'root' as password for user 'root'"
 mysql -uroot -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root'); GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root';"
