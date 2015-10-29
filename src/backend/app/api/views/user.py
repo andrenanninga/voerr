3520c4bba6a044a99bd1bb763413b18cbe7e3d4e
Wesley <wbuurke@gmail.com>
2015-10-29 13:50:47 +0100

    Moved to /api/v1/users/

--- src/backend/app/api/views/user.py
+++ src/backend/app/api/views/user.py
@@ -2,27 +2,3 @@ from flask import Response, json, Blueprint
 from app.api.models.user import User
 
 mod = Blueprint('user', __name__, url_prefix='/api/user')
-
-@mod.route('/<id>', methods=['GET'])
-def getUser(id):
-    try:
-        user = User.query.get(id)
-
-        return Response(json.dumps(user.serialize()), mimetype='application/json')
-
-    except Exception as e:
-        return json.dumps({'error': str(e)})
-
-
-@mod.route('/<id>/email', methods=['GET'])
-def getUserEmail(id):
-    try:
-        user = User.query.get(id)
-        response = {
-            'email': user.email
-        }
-
-        return Response(json.dumps(response), mimetype='application/json')
-
-    except Exception as e:
-        return json.dumps({'error': str(e)})
