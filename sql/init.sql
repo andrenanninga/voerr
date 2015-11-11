323ef5340aa2e4de535f41ef078353ae38168e48
lassebenni <lasse_benninga@hotmail.com>
2015-11-11 15:01:40 +0100

    init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -99,23 +99,205 @@ INSERT INTO allergy (`id`, `name`, `description`) VALUES
 
 -- Insert dish 
 INSERT INTO dish (`id`, `name`, `description`, `cook_id`, `date_created`, `date_updated`) 
-VALUES (1, 'Lamsrack met warme salade van gemengde bonen en langzaam-geroosterde tomaten', 'Mals, mager lamsvlees en een selectie van de beste groenten maakt dit een romantische gerecht dat je niet snel zult vergeten
-', 1, NOW(), NOW()),
+VALUES (1, 'Lamsrack met warme salade van gemengde bonen en langzaam-geroosterde tomaten', 'Lorem markdownum pastor verberis. Haerent et humi manus pressit; quoque
+concretam tenentes guttur in. Pennis tamen. Flumineis levant incertam; sub
+alienae, satia vultus erat cognosse receperat favillae tetigisse tempora calcat.
 
-(2, 'Beef Wellington', 'Een versie van het klassieke steak gerecht - het middelpunt op een speciale gelegenheid.
-', 2, NOW(), NOW()),
+1. Rura nec solacia vitat
+2. Timores an quem profugaque partibus mox
+3. Gener afuerunt genitorque picea inconcessaeque patrias Bellona
 
-(3, 'Citroentaart met zomer bessen', 'Een zingy lemon tart van de heer Ramsay. Gordons versie van de klassieke Franse Tarte au citron is de moeite waard.',
- 3, NOW(), NOW()),
+Cur pondus; miseranda Lucifero ni quoque carpebat Actaea; Iovis? Incumbere exit
+nec putat inque dantem resistere, hoc dixit. Modo haec canos ora in eligit et
+funestaque alas; alto spectare et quoque nota, populabile? Crescunt quondam,
+culta, vindice retusa aquae. Gravius at crede umida; sub tectus mox comes
+aspergine sanguinis bella, limine.
 
-(4, 'Mediterrane zalmfilet', 'Zalm gevuld met mediterrane smaken, dit licht, gezonde voorjaar lunch wordt gemaakt om te delen met vrienden.
+1. Caeneus fluentibus prodiga
+2. At oracula iurgia nondum victa iugis adiutis
+3. Illa sine qui
+
+Quicquam non et tum noceat dimittere divesque conspexit rerum. Me imo gentesque
+inquit, et haut.
+
+Ista murra mutavit regalis draconem in maculat habet; trabes convertunt me adhuc
+Arethusae populi, et ex? Latiis habitare aequora cruento osque rogumque, sors
+mulcet esse. Operitur ipse ora, adsidua nec latens Delius mittantur sese Iunone
+amore, nec silva pietatem sibi. Ducunt potes; sermonibus volatu virum quem
+ferunt, mei hastam preces.
+
+1. Pigeat limitibus sanguine digessit umeri sortita aera
+2. Dicta tibi est nuper quod
+3. Induitur quondam harundine reditus quis
+4. Has illuc est rectior
+
+Sed tibi incertam scabrae, diversis gravem iunctisque est femina mutat magna
+certamine fraternaeque mitra. Hic filia audentem inque; quae odere contra me
+petit inposita. Ter rore trementi, lingua te numenque hausit, Poeantiaden?
+
+Cinyphius locum et sustinet, signumque reus calidusque feroci pater ipsa suisque
+ferinos! Tamen harundine saxo flammis incensaque collecta, inter ense nihil
+unius? Sentit felix palustris et pius, temptat audacissimus intus post levi tua
+manu ambiguus sanguine crescere indignatus ebur inscribit. Aurea de duabus pro
+aberant iam simul Imbreus soli relabi metu fatis, metu hastarum.', 1, NOW(), NOW()),
+
+(2, 'Beef Wellington', 'Lorem markdownum tactaeque minor in dedi ver illis infelix. Peccare baculumque
+pallam precaris exire, nostro potes nec inmitem obstas, calathosque ademptam
+iunxit quam?
+
+1. Voce prius et diu flatuque
+2. Causa tali per
+3. Umquam non omnis exanimata parenti caesas Lelegeia
+4. Levare cultoribus hanc hastamque precor renuente cruoris
+5. Ora regnum deo non
+
+Deos munere libera et lacrimaeque quietum per habere non. Pro enim, pharetram
+faventum tamen statione, sanguine, sonus lucum: me quid monimenta, deus inter
+lenis. Olenios de profers, feres tota quaecumque inguine iterum propera
+consumite ipse spemque transcurrere utile perhorruit distincta consumitis. Soror
+ferarum neve usque est deus late per retractat! Procubuit exanimes sordidus
+bracchia monitisque metas publica, vivum capitum iubet.
+
+- Habebat plangore breve detinuit fuerat per et
+- Et tradidit secantes
+- Rigido pulvereumque sacras suarum
+- Ingentia lampades solito meus tempora iuvenalibus lustrat
+
+Quidem de eumque threicius vesica Baucis vindice barbara si florem Nereida
+canosque Myrrha sertis. Mea cibis nepotum regale pavefacta, mediis, est et
+Cocinthia Apidanosque ambas concutiensque de ut obstrusaque remotis Laomedonteis
+vias! Dumque in ille antra Dictaeo habebat caelamina mora remeasse virtute
+voluntas suo, Pandione anhelanti cerno supereminet dominae Ichnobates exactus.
+
+1. Amnis variatis adhibere dum ullus gemit exiguo
+2. Ad poscit exstinctum surgis
+3. Mollis castra Ardea commota
+4. Firmamina Latonae
+5. Ipse grates triumphis omnis frutices cratera
+6. Cypron superi fugio ferisne limbus Troasque se
+
+Incola rorant, auro dedit ferrumque. Vinum et Nisi Dolona Pactolides margine
+opus gradibus effugit fastigiaque stat, agmine sentibus. Limina perdix leto,
+regnata agit: inpubibus, subitarum nihil: levitate ait, inde nondum, palustres
+si. Adhuc ille; socio ambit, iussit mille videt simul monitisque.
+
+1. Duritia servabant secum non sponte portas satumque
+2. Fronte hic anxius saepe tibi undis fulgorem
+3. Spectarat cernere
+4. Thalamoque verte
+5. Utinamque extemplo Minos toto centauri ut volucris
+
+Tulit veterumque voce exululatque aegre ortus Gorgone At recusat praestant et
+initis turres, vel movit vox, aura? De venerat exitio, venatum, sua et
+Philammon, egerit et functa, de simul praesagia utque Peliaeque patet.', 2, NOW(), NOW()),
+
+(3, 'Citroentaart met zomer bessen', 'Lorem markdownum limes quae, inde annoso quoque patulis vacuum caelaverat
+invergens rupit, Symplegadas sinit vidi usque. Auget in altae! Et neque nigris
+frustra. Praeponere haesit petendi, sine suis lugentis novae, nec refert da
+fallis.
+
+Ut ut habuisse, sanguine et sacra at sinu, fata. Opis ilice flentes: se tu
+cupiens cumulum tanta est patiensque habeo, sperat occupat, minus recludi
+urguere? Viret dixi exstat ea sitim pariter Naryciusque genus verum creatus:
+simul data.
+
+1. Ducebas annorum pinus manusque erat frondes
+2. Diva septem lacrimas copia
+3. Cuius refero melior possit ita forma cibique', 3, NOW(), NOW()),
+
+(4, 'Mediterrane zalmfilet', 'Lorem markdownum fictum, strepitum carus sed lateantque caret sorbere feriuntque
+quibus. Absens in eodem huic feriam, tum sonant Helicen inter, proboque frustra
+parentum aut mento. In aeque inque nigro foedumque, tamen est abstulit.
+
+1. Somnus mihi gravidis sorores mox mellis
+2. Iphin putas
+3. De Cnosiacas templi pudor venientesque
+4. Fieri nec ferarum bello
+5. Erat nota sidera et vota
+
+In fugit aliquid rear utrimque rettulit fides spes mora sanguine carinae.
+Tempora aequatam terras aequora est linguae inopem altera et ceperat ictaque
+sede latitantem, adsensit. Oppida terribiles cunei. Ego veli naidas creditis;
+tum esse, ignibus dubio, deum lacertis at unum frustra. Facias non corpus ait
+levi usque has salve, subitus.
+
+1. Arce cognovit torruit quoque vosne nunc cadunt
+2. Deducens coronatis depulerant eadem tecta Nyseides ordine
+3. Undis mole
+4. Vota vivere detque
+5. Lecti Aiax favent parte concutiens genetricis postquam
+
+Nisi Latina, raptor tela, et altera quidem, est nec, possit? Inde haeret tabula
+quisquis ilice fraternos videri, pressaeque.
+
+Placidos unda cineri color vidi illa lacrimis, et, stant, sceleris inhaesi se
+colonus circum! Volucris sarcina Proetidas Rhexenore ille iuvenum magni radix
+numen dixit silvis alis nec inambitiosa verbis fecit. Bis ope accedere Nesso,
+ubi aura tulit concipiunt equis naris vulnere arbore sepulti nubibus seminaque
+subito sacrificat undae pennae. Misit per iustis rore cuspide laborem dextra
+promptior tumulo penates sanguine faciem.
 ', 4, NOW(), NOW()),
 
-(5, 'Zomer bessen meringues', 'Huisgemaakte zomerbessen meringues. Voor de fijnproever.
-.', 5, NOW(), NOW()),
+(5, 'Zomer bessen meringues', 'Lorem markdownum, adit annos interdum quoque tempus modo, quam meas. Patulos
+Thetidi; sedens corpus tempore caelo mente flecti pulchrosque sibi ablata huius
+crura tumuli? Illa et et solam siccare coniuge, modo magnum et, vota?
 
-(6, 'Pork & Ham Pie', 'Een heerlijke pork pie recept is perfect voor een buffet.
-', 1, NOW(), NOW());
+In sanguine hos domus numquam dimisit cavus crederet cervicem, ab. An putria
+sacra draconum in quis. Citharam nymphas thalamos et verum incerti claudit
+inque, et ultor Iunonis. In inpune et per sequitur tibi in laniavit data.
+
+1. Nullisque sensit Aoniis
+2. Que vetitis et gutture procul auditaque duobus
+3. Armigerae dixerat tectis corpore dabat
+4. Retegente inducta spectabat tamen caelo expers sit
+5. Flebile vestra cruentum rursus recondiderat erat prospexit
+6. Essent denique curru nebulae illo sagittam
+
+Sonant perque, ipse colle fuerat in bellaque quae, poenaededidit vulneret ait.
+Hos amatis singula, virtutem, fraxinus diva, bene et licet spatiantia licet.
+
+1. Suamque qui suis sacra figuram referam
+2. An umbras serpentum
+3. Et iussit suspensus dira clamavit in respicit
+4. Multis recentia Marte si ferunt quae volui
+5. Vellera telum
+6. Nova et tamen nec successu femina
+
+Saepe clipeus, primaque auro si Polymestoris comis; verum nec qui plebemque
+pedum, est. Tot nota tulit parentem: ore Phrygia delicta Creteque ensis,
+indignis tuque illi bellum. Iacui haec ilia lapidum canibus claro proripit
+Cretaei armos sic Danais.
+
+Aptas surgis dedecus Bacchi. Pudor nomen mutataque pressit rupit curvos; aut
+arbore facies sagittis, preme exposcere, sol. Adsim et sumptis gramina taxo..', 5, NOW(), NOW()),
+
+(6, 'Pork & Ham Pie', 'Lorem markdownum fortibus aquis pluviale totidem, tellure ipse vir verbis ferrea
+comitique, hoc. Non humus maternae quid, mira est relictum gurgite. Urbs
+dicuntur crebros pondus te reus, iam sic imbrem, raptor tamen. Ignes Cretenque
+et pater hoc mortisque morbi molarem cornu procellamnos Dianae calido, quem et!
+
+- Tortum descendere inponere
+- Unum satum quod gloria noxque quem rupta
+- Admoto et quo novat qui
+- Ante fluctibus Echion turpe priori emerguntque caeca
+
+Iussis undas auguror meus loqui pluvialibus aure; retenta spes cessit nam! Timeo
+causa cuive: neu sacra et aptumque oculos arguitur?
+
+1. Solum parvae satis
+2. Fugiens certe talibus vitat hoc voce caeli
+3. Pedibusve feriat adorandum sepulcris ille fluunt
+4. Illi cerae viros gemma
+
+Opacas manu; hanc, parentis, diem, anum et expetit erigite videtur armamentis
+nondum, bracchia. Hac opus nunc pallorem bisque obliquis aries.
+
+Prudens tum recens, palmite dixit ad Ulixes cornua cernit superabitur fulgorem
+mittit prope ubi, pro. Hoc pellens terra credunt, plenum iaculatur nec annis
+qualem, silvas in natalis glacies pars, nitore effuge de. Corpora fuere qui
+utque effusus manum nec albas est se Phoebi scrobe. Bobus Caenis navis ieiunia
+includite transibat quicquam cornua primum tenebat Phlegraeis bellare excusat,
+haec.', 1, NOW(), NOW());
 
 
 -- Insert meal 
