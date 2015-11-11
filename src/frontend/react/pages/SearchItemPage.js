3f630af5baebd6f6a8fd599b53f93b08922e159a
Andre Nanninga <ag.nanninga@gmail.com>
2015-11-11 23:51:21 +0100

    changed button text

--- src/frontend/react/pages/SearchItemPage.js
+++ src/frontend/react/pages/SearchItemPage.js
@@ -119,7 +119,7 @@ export default class SearchItemPage extends React.Component {
 				<div className="meal">
 					<h4>&euro;{price} per maaltijd</h4>
 					<p>Vanavond tussen <strong>{availableFrom}</strong> en <strong>{availableUntil}</strong></p>
-					<button className="button-primary">Reservering maken</button>
+					<button className="button-primary">Bord bestellen</button>
 				</div>
 			</div>
 
